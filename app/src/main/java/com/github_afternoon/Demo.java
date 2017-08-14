4ee7b43b03b4c4fd9fa6ed966214ede4a5ad0382
Windows <lingling>
2017-08-14 18:34:39 +0800

    第二次提交
    时间:18:34
    
    提交人:聂玲玲

---
+++ app/src/main/java/com/github_afternoon/Demo.java
@@ -0,0 +1,13 @@
+package com.github_afternoon;
+
+import android.util.Log;
+
+/**
+ * Created by Windows on 2017/8/14.
+ */
+
+public class Demo {
+    private void demo(){
+        Log.e("TAG","第一次修改代码");
+    }
+}
