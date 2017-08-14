70e3ad308f146134696144d02dd7d23f9c40b057
Windows <lingling>
2017-08-14 19:17:32 +0800

    第四次提交
    时间:19:17
    
    提交人:聂玲玲

---
+++ app/src/main/java/com/github_afternoon/Test.java
@@ -0,0 +1,13 @@
+package com.github_afternoon;
+
+import android.util.Log;
+
+/**
+ * Created by Windows on 2017/8/14.
+ */
+
+public class Test {
+    public void newTest(){
+        Log.e("TAG","第二次添加代码");
+    }
+}
