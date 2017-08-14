55e95c2c83b42193360d7a4aebe19331b42583ca
Windows <lingling>
2017-08-14 23:17:32 +0800

    第七次提交
           时间:23:17
    
           提交人:聂玲玲

--- app/src/main/java/com/github_afternoon/Demo03.java
+++ app/src/main/java/com/github_afternoon/Demo03.java
@@ -5,5 +5,18 @@ package com.github_afternoon;
  */
 
 public class Demo03 {
+    private static Demo03 demo03;
+    private Demo03(){
+    }
 
+    public static Demo03 getInstance(){
+        if(demo03==null){
+            synchronized (Demo03.class){
+                if (demo03==null){
+                    demo03=new Demo03();
+                }
+            }
+        }
+        return demo03;
+    }
 }
